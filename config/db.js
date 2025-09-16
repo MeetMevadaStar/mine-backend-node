b283db588777fd45947447de44211124752ce973
meetFeeltech <meet.feeltechsolution@outlook.com>
2025-09-16 17:04:26 +0530

    test changes

--- config/db.js
+++ config/db.js
@@ -6,15 +6,13 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false, // Render requires SSL
-  },
 });
 
 pool
   .connect()
-  .then(() => {
+  .then((client) => {
     console.log("✅ PostgreSQL Connected!");
+    client.release();
   })
   .catch((err) => {
     console.error("❌ PostgreSQL Connection Failed:", err.message);
