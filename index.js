0669173c26a59c97eb1bcbc414c3d741c5ebb418
meetFeeltech <meet.feeltechsolution@outlook.com>
2025-09-16 17:14:09 +0530

    changes

--- index.js
+++ index.js
@@ -12,12 +12,9 @@ app.use(express.json());
 // Route grouping
 app.use("/api/v1", v1Routes);
 
-app.get("/api", (req, res) => {
-  res.send("Server is running ✅");
-});
-
-// 404 handler
-app.use((req, res) => {
+// //* Server
+app.use((req, res, next) => {
+  req.APINotFound = true;
   return res.status(404).json({
     error: "Not Found",
     message: `EndPoint not available: ${req.method} ${
@@ -26,37 +23,6 @@ app.use((req, res) => {
   });
 });
 
-module.exports = app;
-
-// require("dotenv").config();
-// const express = require("express");
-// const cors = require("cors");
-
-// const app = express();
-// const v1Routes = require("./routes/v1");
-
-// const PORT = process.env.PORT || 5000;
-// const HOST = process.env.HOST || "localhost";
-
-// // Middlewares
-// app.use(cors());
-// app.use(express.json());
-
-// // Route grouping
-// app.use("/api/v1", v1Routes); // ✅ This is enough
-
-// // //* Server
-// app.use((req, res, next) => {
-//   req.APINotFound = true;
-//   return res.status(404).json({
-//     error: "Not Found",
-//     message: `EndPoint not available: ${req.method} ${
-//       req.originalUrl?.split("?")[0]
-//     }`,
-//   });
-// });
-
-// // Start server
-// app.listen(PORT, HOST, () => {
-//   console.log(`🚀 Server is running at http://${HOST}:${PORT}`);
-// });
+app.get("/api", (req, res) => {
+  res.send("Server is running ✅");
+});
